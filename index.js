6a08dc69b3bfda08ffbe0e5af6b050a88a77316f
Parp Annop N Ayuttaya <61160112@go.buu.ac.th>
2021-03-04 19:46:29 +0700

    commit 3

--- index.js
+++ index.js
@@ -1,43 +1,64 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectId
 const app = express()
 
 app.use(express.json())
-let books = []
+const url = 'mongodb+srv://superadmin:kaew33448@cluster0.8sjt0.mongodb.net/books?retryWrites=true&w=majority'
+const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
-app.get('/books', (req, res) => {
+
+async function connect(){
+    await client.connect()
+    db = client.db('books')
+    booksConllection = db.collection('books')
+}
+connect()
+
+app.get('/books', async (req, res) => {
     //input
 
     //process
+    const cursor = await moviesConllection.find({})
+    const result = await cursor.toArray()
     //output
     res.status(200).json(books)
 
 })
 
-app.post('/books',(req, res) => {
-    // input
+app.post('/books', async (req, res) => {
+
     let newTitle = req.body.title
     let newPrice = req.body.price
     let newUnit = req.body.unit
     let newIsbn = req.body.isbn
-    let newImage = req.body.image
+    let newImageurl  = req.body.imageurl
 
-    
-   
-    // key: value
-    let newBooks ={
+
+    let newBooks = {
         title: newTitle,
         price: newPrice,
         unit: newUnit,
         isbn: newIsbn,
-        image : newImage
+        imageurl : newImageurl
     }
-    let BooksID = 0
-    // process
-    Books.push(newBooks)
-    //n-1
-    BooksID = Books.length - 1
-    // output
-    res.status(201).json(BooksID)
+    let booksID = 0
+
+    const result = await booksCollection.insertOne(newBooks)
+    booksID = result.insertedId
+    res.status(201).json(booksID)
 })
+
+
+app.get('/books/:id',async (req, res) => {
+
+    let id = req.params.id
+
+    const books = await booksCollection.findOne({ _id: ObjectId(id)})
+
+
+    res.status(200).json(books)
+})
+
 const port = 3000
-app.listen(port, () => console.log(`Server started again at ${port}`))
+app.listen(port,  () => console.log(`Server started at ${port}`))
